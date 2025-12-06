2293b259a99933039208636ee1f1699c547779b7
Lumine2024 <diaochan2021@outlook.com>
2025-12-06 12:41:47 +0000

    Refactor to use std::shared_ptr and value semantics, reduce pressure test iterations
    Co-authored-by: Lumine2024 <149254783+Lumine2024@users.noreply.github.com>

--- include/node_manager.h
+++ include/node_manager.h
@@ -5,6 +5,8 @@
 #include "lru.h"
 #include "pager.h"
 #include <string>
+#include <cstring>
+#include <memory>
 
 namespace bplus_sql {
 
@@ -12,50 +14,79 @@ class NodeManager {
 public:
     NodeManager(const std::string &fileName) : m_lru(), m_pager(fileName) {}
     ~NodeManager() {
-        // We need to write all nodes in m_lru to m_pager
-        // We need a function in LRUCache like:
-        /*
-        void traverse(auto &&fn) {
-            for(auto [pageId, pair_of_node_and_iterator] : m_cache) {
-                fn(pageId, pair_of_node_and_iterator.first);
-            }
-        }
-        */
-        // so that we can call that in destruction function:
-        /*
-        m_lru.traverse([&](size_t pageId, BPlusNode *node) {
-            m_pager.writePage(pageId, node);
+        // Write all nodes in m_lru back to m_pager
+        m_lru.traverse([&](size_t pageId, std::shared_ptr<BPlusNode> node) {
+            m_pager.writePage(pageId, *node);
         });
-        */
     }
     NodeManager(const NodeManager &) = delete;
     NodeManager &operator=(const NodeManager &) = delete;
-    BPlusNode *getNode(size_t pageId) {
+    
+    // Return a copy of the node - caller doesn't need to manage memory
+    BPlusNode getNode(size_t pageId) {
         if(m_lru.contains(pageId)) {
-            return m_lru.get(pageId);
+            // Return a copy of the cached node
+            auto cachedNode = m_lru.get(pageId);
+            return *cachedNode;
         } else {
-            auto result = new BPlusNode();
-            m_pager.readPage(pageId, *result);
-            // We need to get the tail of m_lru to write to m_pager
-            // We need this in LRUCache:
-            /*
-            std::pair<std::size_t, BPlusNode *> tail() const {
-                size_t backId = m_list.back();
-                return std::make_pair(backId, m_cache[backId].first);
+            BPlusNode result;
+            m_pager.readPage(pageId, result);
+            
+            // Check if we're at capacity and need to evict
+            if(m_lru.size() >= LRUCache::CAPACITY) {
+                // Get the tail node (least recently used) and write it back to pager
+                auto [tailPageId, tailNode] = m_lru.tail();
+                if(tailNode != nullptr) {
+                    m_pager.writePage(tailPageId, *tailNode);
+                }
             }
-            */
-            m_lru.put(pageId, result);
+            
+            // Store a copy in cache
+            m_lru.put(pageId, std::make_shared<BPlusNode>(result));
+            
             return result;
         }
     }
-    void putNode(size_t pageId, BPlusNode *node) {
+    
+    // Update the cache with the node value
+    void putNode(size_t pageId, const BPlusNode &node) {
+        // Update the cache with a copy of the node
+        auto cachedCopy = std::make_shared<BPlusNode>(node);
+        
         if(m_lru.contains(pageId)) {
-            m_lru.put(pageId, node);
+            m_lru.put(pageId, cachedCopy);
         } else {
-            // The same tail() function needed in LRUCache
-            m_lru.put(pageId, node);
+            // Check if we're at capacity and need to evict
+            if(m_lru.size() >= LRUCache::CAPACITY) {
+                // Get the tail node (least recently used) and write it back to pager
+                auto [tailPageId, tailNode] = m_lru.tail();
+                if(tailNode != nullptr) {
+                    m_pager.writePage(tailPageId, *tailNode);
+                }
+            }
+            m_lru.put(pageId, cachedCopy);
         }
     }
+    
+    // Expose pager methods for metadata operations
+    template<typename T>
+    void writeMetadata(const T& metadata) {
+        m_pager.writeMetadata(metadata);
+    }
+    
+    template<typename T>
+    void readMetadata(T& metadata) {
+        m_pager.readMetadata(metadata);
+    }
+    
+    bool fileExists() const {
+        return m_pager.fileExists();
+    }
+    
+    size_t getFileSize() {
+        return m_pager.getFileSize();
+    }
+    
 private:
     LRUCache m_lru;
     Pager m_pager;
